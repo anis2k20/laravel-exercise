9034ffcba2088fc5b73dc3756e751ef7d44061ad
Anis <mdanismia992@gmail.com>
2025-10-01 08:45:00 +0600

    feat: add HasMany relation

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,25 @@
 <?php
 
 use App\Models\Phone;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Models\Comment;
 
 Route::get('/', function () {
 
-    $users = User::all();
+//    $users = User::all();
 //    $phone = User::find(1)->phone;
+
+    $posts = Post::all();
+
+//    $comment = Post::find(2)->comments;
+
+    $post = Comment::find(4)->post;
+
+    return $post;
 //
 //    $user = Phone::find(1)->user;
 //    return $user.'-'. $phone;
-    return view('index', compact('users'));
+//    return view('index', compact('users'));
 });
